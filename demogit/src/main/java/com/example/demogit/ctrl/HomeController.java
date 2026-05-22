fd1ecb55a5aeaa2c4ef1b53f2d08a9382c0566f9
suho98 <qusql98@naver.com>
2026-05-22 13:22:46 +0900

    feat : 13번째줄 변경

--- demogit/src/main/java/com/example/demogit/ctrl/HomeController.java
+++ demogit/src/main/java/com/example/demogit/ctrl/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping("/")
-	public String getUserName(@RequestParam String param) {
+	public String getMethodName(@RequestParam String param) {
 	return "좋은아침 입니다.";
 }
 }	
